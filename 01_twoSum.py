f35928e155065e044a05e72c02e8ee9302aa3ca7
Haojing Tong <haojing.tong@outlook.com>
2023-12-11 23:29:38 +0000

    Two Sum

---
+++ 01_twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_map = {} # Create a dictionary to store number: index pairs
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in index_map:
+                return [index_map[complement], i]  # Found the pair
+            index_map[num] = i # Store the element of current 
+        return []
+    
